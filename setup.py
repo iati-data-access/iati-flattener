a0f16a8f1c18152dbc919b10bf4878ef9ba854c2
Mark Brough <mark.brough@gmail.com>
2021-02-18 18:47:40 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ requirements = """
 lxml>=4.6.1
 requests>=2.24.0
 exchangerates>=0.3.4
-pandas>=1.1.4
-numpy>=1.19.4
-PyExcelerate>=0.9.0
-openpyxl>=3.0.5
+pandas==1.1.4
+numpy==1.19.4
+PyExcelerate==0.9.0
+openpyxl==3.0.5
 xlrd==1.2.0
 iatikit>=2.3.0
 """
@@ -15,7 +15,7 @@ iatikit>=2.3.0
 setup(
     name="iatiflattener",
     packages=find_packages(exclude=['ez_setup', 'examples']),
-    version='0.1.5b2',
+    version='0.1.5b3',
     description="A set of tools to flatten IATI data.",
     author="Mark Brough",
     author_email="mark@brough.io",
