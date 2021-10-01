24932652814d9692e3a80a69d5611f0bfd429e03
Mark Brough <mark.brough@gmail.com>
2021-10-01 14:14:45 +0200

    Make headers multilingual

--- iatiflattener/lib/variables.py
+++ iatiflattener/lib/variables.py
@@ -1,77 +1,146 @@
+from collections import OrderedDict
 import numpy as np
 
 DPORTAL_URL = "https://d-portal.org/q.html?aid={}"
 
-CSV_HEADERS = [
-    'iati_identifier',
-    'title',
-    'reporting_org',
-    'reporting_org_type',
-    'aid_type',
-    'finance_type',
-    'flow_type',
-    'provider_org',
-    'provider_org_type',
-    'receiver_org',
-    'receiver_org_type',
-    'transaction_type',
-    'value_original', 'currency_original',
-    'value_usd', 'exchange_rate_date',
-    'exchange_rate', 'transaction_date',
-    'country_code', 'multi_country',
-    'sector_category', 'sector_code',
-    'fiscal_year', 'fiscal_quarter',
-    'fiscal_year_quarter', 'url']
 
-_DTYPES = [str, str, str, str,
-                 str, str, str, str, str,
-                 str, str, str, str,
-                 str, np.float64, str, str,
-                 str, str,
-                 np.int32, str, str,
-                 np.int32, str, str, str]
+HEADERS = OrderedDict({
+   'iati_identifier': str,
+   'title': str,
+   'reporting_org': str,
+   'reporting_org_type': str,
+   'aid_type': str,
+   'finance_type': str,
+   'flow_type': str,
+   'provider_org': str,
+   'provider_org_type': str,
+   'receiver_org': str,
+   'receiver_org_type': str,
+   'transaction_type': str,
+   'value_original': str,
+   'currency_original': str,
+   'value_usd': np.float64,
+   'exchange_rate_date': str,
+   'exchange_rate': str,
+   'value_eur': np.float64,
+   'value_local': np.float64,
+   'transaction_date': str,
+   'country_code': str,
+   'multi_country': np.int32,
+   'sector_category': str,
+   'sector_code': str,
+   'humanitarian': np.int32,
+   'fiscal_year': np.int32,
+   'fiscal_quarter': str,
+   'fiscal_year_quarter': str,
+   'url': str
+})
 
-GROUP_BY_HEADERS = [
-   'iati_identifier',
+
+MULTILANG_HEADERS = [
    'title',
    'reporting_org',
+   'provider_org',
+   'receiver_org'
+]
+
+
+def headers_with_langs(langs):
+   out = []
+   for header in HEADERS.keys():
+      if header in MULTILANG_HEADERS:
+         out += ['{}#{}'.format(header, lang) for lang in langs]
+      else:
+         out += [header]
+   return out
+
+
+def dtypes_with_langs(langs):
+   out = []
+   for dtype in HEADERS.values():
+      if dtype in MULTILANG_HEADERS:
+         out += [dtype for lang in langs]
+      else:
+         out += [dtype]
+   return out
+
+
+def group_by_headers_with_lang(lang):
+   return [
+   'iati_identifier',
+   'title#{}'.format(lang),
+   'reporting_org#{}'.format(lang),
    'reporting_org_type',
    'aid_type',
    'finance_type',
    'flow_type',
-   'provider_org',
+   'provider_org#{}'.format(lang),
    'provider_org_type',
-   'receiver_org',
+   'receiver_org#{}'.format(lang),
    'receiver_org_type',
    'transaction_type',
    'country_code',
    'multi_country',
    'sector_category',
    'sector_code',
+   'humanitarian',
    'fiscal_year',
    'fiscal_quarter',
    'fiscal_year_quarter',
    'url']
 
-OUTPUT_HEADERS = [
-   'IATI Identifier',
-   'Title',
-   'Reporting Organisation',
-   'Reporting Organisation Type',
-   'Aid Type',
-   'Finance Type',
-   'Flow Type',
-   'Provider Organisation',
-   'Provider Organisation Type',
-   'Receiver Organisation',
-   'Receiver Organisation Type',
-   'Transaction Type',
-   'Recipient Country or Region',
-   'Multi Country',
-   'Sector Category',
-   'Sector',
-   'Calendar Year',
-   'Calendar Quarter',
-   'Calendar Year and Quarter',
-   'URL',
-   'Value (USD)']
+
+OUTPUT_HEADERS = {
+   'en': [
+      'IATI Identifier',
+      'Title',
+      'Reporting Organisation',
+      'Reporting Organisation Type',
+      'Aid Type',
+      'Finance Type',
+      'Flow Type',
+      'Provider Organisation',
+      'Provider Organisation Type',
+      'Receiver Organisation',
+      'Receiver Organisation Type',
+      'Transaction Type',
+      'Recipient Country or Region',
+      'Multi Country',
+      'Sector Category',
+      'Sector',
+      'Humanitarian',
+      'Calendar Year',
+      'Calendar Quarter',
+      'Calendar Year and Quarter',
+      'URL',
+      'Value (USD)',
+      'Value (EUR)',
+      'Value (Local currrency)'
+   ],
+   'fr': [
+      'Identifiant de l’IITA',
+      'Titre',
+      'Organisme déclarant',
+      'Type d’organisme déclarant',
+      'Type d’aide',
+      'Type de financement',
+      'Type d’apport',
+      'Organisme prestataire',
+      'Type d’organisme prestataire',
+      'Organisme bénéficiaire',
+      'Type d’organisme bénéficiaire',
+      'Type de transaction',
+      'Pays ou région bénéficiaire',
+      'Multipays',
+      'Catégorie de secteur',
+      'Secteur',
+      'Humanitarian',
+      'Année civile',
+      'Trimestre civil',
+      'Année et Trimestre civile',
+      'URL',
+      'Valeur (USD)',
+      'Valeur (EUR)',
+      'Valeur (Monnaie locale)'
+   ]
+}
