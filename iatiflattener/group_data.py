264cdc05a3428ddf6134ce73863d661ed4883d9b
Ben Webb <ben.webb@opendataservices.coop>
2022-02-16 15:04:27 +0000

    Restrict GroupFlatIATIData to given country codes

--- iatiflattener/group_data.py
+++ iatiflattener/group_data.py
@@ -28,8 +28,12 @@ class GroupFlatIATIData():
             sector_req = self.get_codelist_with_fallback(lang, "Sector")
             sector_groups_req = self.get_codelist_with_fallback(lang, "SectorGroup")
 
-            country_names = dict(map(lambda country: (country['code'], country['name']), country_req.json()["data"]))
-            region_names = dict(map(lambda region: (region['code'], region['name']), region_req.json()["data"]))
+            if self.country_codes:
+                country_names = dict(map(lambda country: (country['code'], country['name']), filter(lambda country: country['code'] in self.country_codes, country_req.json()["data"])))
+                region_names = dict(map(lambda region: (region['code'], region['name']), filter(lambda region: region['code'] in self.country_codes, region_req.json()["data"])))
+            else:
+                country_names = dict(map(lambda country: (country['code'], country['name']), country_req.json()["data"]))
+                region_names = dict(map(lambda region: (region['code'], region['name']), region_req.json()["data"]))
             country_names.update(region_names)
             self.country_names[lang] = country_names
             sector_names = dict(map(lambda country: (country['code'], country['name']), sector_req.json()["data"]))
@@ -182,8 +186,9 @@ class GroupFlatIATIData():
                 }, json_file)
         print("FINISHED PROCESS AT {}".format(datetime.datetime.utcnow()))
 
-    def __init__(self, langs=['en']):
+    def __init__(self, langs=['en'], country_codes=[]):
         self.langs = langs
+        self.country_codes = country_codes
         self.CSV_HEADERS = variables.headers(langs)
         self._DTYPES = variables.dtypes(langs)
         self.CSV_HEADER_DTYPES = dict(map(lambda csv_header: (csv_header[1], self._DTYPES[csv_header[0]]), enumerate(self.CSV_HEADERS)))
